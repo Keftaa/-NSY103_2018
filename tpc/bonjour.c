1b1f2b66717674cd5b61fbe149c1cf9d525dbe89
keftaa <adnan.hamzeh@isae.edu.lb>
2017-11-03 15:03:45 +0200

    Started working on TP3 Introduction a C

---
+++ tpc/bonjour.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Affiche la chaine de caractere "Bonjour le monde\n" sur stdout.
+    // \n est un retour a la ligne
+    printf("Bonjour le monde\n");
+    return 0;
+}
---
+++ tpc/partie 2/lec_ec.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Entrez un caractere : ");
+    c = getchar();
+
+    printf("Voici le caractere entre : %c - %d\n", c);
+    return 0;
+}
